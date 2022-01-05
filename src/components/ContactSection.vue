c7f566249b78157a6ce9d76b71083360de257c1e
caneto123 <75550601+caneto123@users.noreply.github.com>
2022-01-04 23:24:32 -0300

    PhoneRules
    PhoneRules

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -111,17 +111,17 @@ export default {
     valid: true,
     name: "",
     nameRules: [
-      (v) => !!v || "O campo nome é obrigatório",
-      (v) => (v && v.length >= 6) || "O nome precisa ter mais de 6 caracteres",
+      (v) => !!v || "Este campo es obligatorio",
+      (v) => (v && v.length >= 6) || "El nombre precisa tener más de 6 caracteres",
     ],
     email: "",
     emailRules: [
-      (v) => !!v || "O campo email é obrigatório",
-      (v) => /.+@.+\..+/.test(v) || "O E-mail precisa ser válido",
+      (v) => !!v || "Este campo es obligatorio",
+      (v) => /.+@.+\..+/.test(v) || "EL E-mail precisa ser válido",
     ],
     textArea: "",
     textAreaRules: [
-      (v) => !!v || "O campo de texto é obrigatório",
+      (v) => !!v || "Este campo es obligatorio",
       (v) => (v && v.length >= 10) || "Mínimo de 10 caracteres",
     ],
     lazy: false,
